697c59a1993d831404886f7fd190aaf3ede11112
Guillaume Reinert <55312880+GReinert11@users.noreply.github.com>
2023-03-20 21:29:38 +0100

    Ajout tests unitaire

--- OffreService/src/test/java/fr/ul/miage/OffreService/OffreService/OffreServiceApplicationTests.java
+++ OffreService/src/test/java/fr/ul/miage/OffreService/OffreService/OffreServiceApplicationTests.java
@@ -1,13 +1,108 @@
 package fr.ul.miage.OffreService.OffreService;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.sql.Date;
+import java.util.UUID;
 
-@SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+import fr.ul.miage.OffreService.boundary.OffreStageAssembler;
+import fr.ul.miage.OffreService.boundary.OffreStageRepository;
+import fr.ul.miage.OffreService.controllers.OffreStageController;
+import fr.ul.miage.OffreService.entity.OffreStage;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+
+
+
+
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, properties = {
+	"spring.datasource.url=jdbc:h2:mem:m2db;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+	"spring.datasource.driver-class-name=org.h2.Driver",
+	"spring.datasource.username=sa",
+	"spring.datasource.password=password",
+	"spring.jpa.hibernate.ddl-auto=create-drop",	
+	"spring.jpa.defer-datasource-initialization=true",
+	"spring.jpa.database-plateform=org.hibernate.dialect.H2Dialectx",
+	"spring.h2.console.enabled=true",
+	"spring.jpa.hibernate.ddl-auto=update"})
 class OffreServiceApplicationTests {
+
+	@Autowired
+	private OffreStageRepository offreRepository;
+
+	@Autowired
+	private OffreStageAssembler offreAssembler;
+
+	@Autowired
+    private OffreStageController offreController;
+
 
 	@Test
 	void contextLoads() {
 	}
+
+	@BeforeEach
+	public void setupContext() {
+		offreRepository.deleteAll();
+		//RestAssured.port = port;
+	}
+
+
+	@Test
+	 void getOneOffre(){
+	
+		String test = "3c0969ac-c6e3-40f2-9fc8-2a59b8987918";
+		UUID uuid = UUID.fromString(test);
+		String a = uuid.toString();
+		UUID uuid2 = UUID.randomUUID();
+		String str="2023-02-01";  
+		String str2="2023-04-01";  
+		Date d1 = Date.valueOf(str);
+		Date d2 = Date.valueOf(str2);
+        OffreStage offre = new OffreStage("test"
+		, "domaine"
+		,"test decription"
+		,d1
+		,"1"
+		,"2"
+		,d2
+		,"2"
+		,3
+		,"2"
+		,uuid2,"France"
+		,"Metz"
+		,57000
+		,"1 rue"
+		,2.4
+		,2.3
+		,"95555"
+		,"aaaa");
+        offre.setId(uuid);
+		offreRepository.save(offre);
+		when().get("/offres/3c0969ac-c6e3-40f2-9fc8-2a59b8987918").then().statusCode(HttpStatus.SC_OK)
+		.and()
+		.assertThat()
+		.body("id",equalTo(uuid));
+        
+		
+		/*Mockito.when(offreRepository.findById(uuid)).thenReturn(Optional.of(offre));
+        Mockito.when(offreAssembler.toModel(offre)).thenReturn(new OffreModel());
+
+        ResponseEntity<?> response = offreController.getOffreById(uuid);
+
+        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assert.assertTrue(response.getBody() instanceof OffreModel);*/
+
+	 }
 
 }
--- User-Service/src/test/java/fr/ul/miage/UserService/UserServiceApplicationTests.java
+++ User-Service/src/test/java/fr/ul/miage/UserService/UserServiceApplicationTests.java
@@ -3,6 +3,7 @@ package fr.ul.miage.UserService;
 import static io.restassured.RestAssured.when;
 
 import org.apache.http.HttpStatus;
+import org.apache.http.protocol.HTTP;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,18 +13,26 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static org.hamcrest.CoreMatchers.*;
 
+import java.util.UUID;
+
 import fr.ul.miage.UserService.boundary.UsersRepository;
 import fr.ul.miage.UserService.entity.Users;
 import io.restassured.RestAssured;
 import fr.ul.miage.UserService.controllers.UsersController;
+
+
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, properties = {
 	"spring.datasource.url=jdbc:h2:mem:m2db;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
 	"spring.datasource.driver-class-name=org.h2.Driver",
 	"spring.datasource.username=sa",
 	"spring.datasource.password=password",
-	"spring.jpa.hibernate.ddl-auto=create-drop",
-	"spring.jpa.defer-datasource-initialization=true"})
+	"spring.jpa.hibernate.ddl-auto=create-drop",	
+	"spring.jpa.defer-datasource-initialization=true",
+	"spring.jpa.database-plateform=org.hibernate.dialect.H2Dialectx",
+	"spring.h2.console.enabled=true",
+	"spring.jpa.hibernate.ddl-auto=update"})
+
 class UserServiceApplicationTests {
 
 	@LocalServerPort
@@ -31,7 +40,14 @@ class UserServiceApplicationTests {
 
 	@Autowired
 	UsersRepository ur;
-	UsersController userController;
+
+	@Autowired
+    private UsersRepository ur2;
+
+
+
+
+
 
 	@BeforeEach
 	public void setupContext() {
@@ -48,11 +64,80 @@ class UserServiceApplicationTests {
 		when().get("/Users").then().statusCode(HttpStatus.SC_OK);
 	}
 
-	@Test
-	void getAllUsers() {
-		Users user = new Users("soudart", "1 rue de oui", "serge@oudart.com", "Oudart", "Serge");
+ 	/*@Test
+	 void getAllUsers() {
+		 Users user = new Users("soudart", "1 rue de oui", "serge@oudart.com", "Oudart", "Serge");
+		 Users user2 = new Users("soudart2", "1 rue de oui", "serge2@oudart.com", "Oudart2", "Serge2");
+		 Users user3 = new Users("soudart3", "1 rue de oui", "serge3@oudart.com", "Oudart3", "Serge3");
+		 ur.save(user);
+		 ur.save(user2);
+		 ur.save(user3);
+		 when().get("/Users").then().statusCode(HttpStatus.SC_OK)
+			 .and().assertThat().body("size()",equalTo(3));
+	 }*/
+
+	/*  @Test
+	 void getOneUser(){
+		UUID id = UUID.randomUUID();
+		Users user = new Users(id,"soudart", "1 rue de oui", "serge@oudart.com", "Oudart", "Serge");
 		ur.save(user);
-		when().get("/Users").then().statusCode(HttpStatus.SC_OK)
-            .and().assertThat().body("size()",equalTo(1));
-	}
+		when().get("/Users/{id}").then().statusCode(HttpStatus.SC_OK)
+		.and().assertThat().body("id",equalTo(id))
+		.body("username",equalTo("soudart"))
+		.body("adresse",equalTo("1 rue de oui"))
+		.body("email",equalTo("serge@oudart.com"))
+		.body("nom",equalTo("Oudart"))
+		.body("prenom",equalTo("Serge"));
+
+	 }*/
+
+/* 	@Test
+	public void getAllUsersAPI() throws Exception 
+{
+	Users user = new Users("soudart", "1 rue de oui", "serge@oudart.com", "Oudart", "Serge");
+	ur.save(user);
+  	mvc.perform(MockMvcRequestBuilders
+  			.get("/Users")
+  			.accept(MediaType.APPLICATION_JSON))
+      .andDo(print())
+      .andExpect(status().isOk())
+      .andExpect(MockMvcResultMatchers.jsonPath("$.Users").exists())
+      .andExpect(MockMvcResultMatchers.jsonPath("$.Users[*].id").isNotEmpty());
+}*/
+
+
+ /* 	@Test 
+	public void testReturnAll(){
+
+		Users user = new Users("test1", "1 rue de oui", "serge@oudart.com", "Oudart", "Serge");
+		Users user3 = new Users("test2", "1 rue de oui", "usertest2@oudart.com", "nom2", "prenom2");
+		List<Users> userList = new ArrayList<>();
+		userList.add(user);
+		userList.add(user3);
+		ur.save(user);
+		ur.save(user3);
+
+		Mockito.when(ur.findAll()).thenReturn(userList);
+		//Mockito.when(userAssembler.toCollectionModel(userList)).thenReturn(new CollectionModel<>(userList));
+
+		ResponseEntity<?> response = userController.getAllUsers();
+      	assertEquals(HttpStatus.SC_OK, response.getStatusCode());
+      	assertNotNull(response.getBody());
+
+
+	}*/
+
+	/*@Test
+	public void testfindAll() throws Exception {
+	  Users user = new Users("test1", "1 rue de oui", "serge@oudart.com", "Oudart", "Serge");
+	  List<Users> userList = Arrays.asList(user);
+   
+	  Mockito.when(ur.findAll()).thenReturn(userList);
+   
+	  mockMvc.perform(get("/Users"))
+		  .andExpect(status().isOk())
+		  .andExpect(jsonPath("$", Matchers.hasSize(1)))
+		  .andExpect(jsonPath("$[0].username", Matchers.is("test1")))
+		  .andReturn();
+	}*/
 }
